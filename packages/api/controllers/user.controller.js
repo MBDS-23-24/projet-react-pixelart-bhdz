1120fb9d7a052f38e173703867132731fb92cae7
Quentin Hoareau <64120596+quentinhoareau@users.noreply.github.com>
2024-02-11 15:19:10 +0100

    #24 : update schema.prisma

--- packages/api/controllers/user.controller.js
+++ packages/api/controllers/user.controller.js
@@ -3,6 +3,6 @@ import {catchError} from "../error/error-handler.js";
 
 export const helloWorld = async (req, res, next) => {
     await catchError(async () => {
-        res.send(await prisma.user.findMany())
+        res.send('Hello World')
     }, next)
 }
