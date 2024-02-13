32335052fd7c46f5589aa1e2e56a57594f91cf82
Yehoudi <vincentyehoudi7@gmail.com>
2024-02-13 13:54:43 +0100

    Modification du fichier Profile.jsx

--- packages/client/src/pages/Profile/Background.jsx
+++ packages/client/src/pages/Profile/Background.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
-import './Profile.scss';
-
-const Background = () => {
+import './Profile.scss'
+export default function Background(){
     return (
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1600 800"
              className="fixed left-0 top-0 min-h-screen min-w-screen illustration" preserveAspectRatio="none">
@@ -17,5 +15,3 @@ const Background = () => {
         </svg>
     )
 }
-
-export { Background }; 
--- packages/client/src/pages/Profile/Profile.jsx
+++ packages/client/src/pages/Profile/Profile.jsx
@@ -1,44 +1,48 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Title, Text, Button } from "@mantine/core";
-import { Background } from "./Background.jsx";
+import { UserContext } from "../../provider/UserContext.jsx";
+//import { Background } from "./Background.jsx";
 import './Profile.scss';
-//import {IconCheck, IconLock, IconMailFilled} from "@tabler/icons-react";
-//import {Button, Text, TextInput, Title} from "@mantine/core";
 import GenericProfileSvg from './GenericProfileSvg.jsx';
-
+import { IconUser, IconMailFilled, IconEdit } from "@tabler/icons-react";
 
 export default function Profile() {
-    const { user } = useContext(UserContext);
-  
+    const { user, setUser } = useContext(UserContext);
+    // Données statiques à afficher dans le profil
+    const staticData = {
+        username: "Yehoudi",
+        email: "vincentyehoudi@example.com",
+    };
+
     return (
-      <div className="profile">
-        <Background />
-        <div className="card">
-          <div>
-            <Title order={3} className={"b-600"}>
-              {user.username}'s Profile
-            </Title>
-            <Text className={"b-300"} size="sm" color="dimmed">
-              Your profile information
-            </Text>
-          </div>
-          <div className="profile-info">
-            <div className="profile-field">
-              <IconUser size={24} />
-              <Text>{user.username}</Text>
+        <div className="profile">
+            {/* {<Background />} */}
+            <div className="card">
+                <div>
+                    <Title order={3} className={"b-600"}>
+                        {staticData.username}'s Profile
+                    </Title>
+                    <Text className={"b-300"} size="sm" color="dimmed">
+                        Your profile information
+                    </Text>
+                </div>
+                <div className="profile-info">
+                    <GenericProfileSvg />
+                    <div className="profile-field">
+                        <IconUser size={24} />
+                        <Text>{staticData.username}</Text>
+                    </div>
+                    <div className="profile-field">
+                        <IconMailFilled size={24} />
+                        <Text>{staticData.email}</Text>
+                    </div>
+                </div>
+
+                <Button fullWidth>
+                    <IconEdit size={18} style={{ marginRight: 8 }} />
+                    Update Profile
+                </Button>
             </div>
-            <div className="profile-field">
-              <IconMailFilled size={24} />
-              <Text>{user.email}</Text>
-            </div>
-            {/* Add more fields as needed for your user profile */}
-          </div>
-  
-          <Button fullWidth>
-            <IconEdit size={18} style={{ marginRight: 8 }} />
-            Update Profile
-          </Button>
         </div>
-      </div>
     );
-  }
+}
