ccd971b53a7167a3a700e709f2369e571c652d7c
Yehoudi <vincentyehoudi7@gmail.com>
2024-02-13 17:31:21 +0100

    Preparation de la page modif de l'utilisateur

--- packages/client/src/pages/Profile/Profile.jsx
+++ packages/client/src/pages/Profile/Profile.jsx
@@ -1,56 +1,33 @@
-import { useContext } from 'react';
-import { Title, Text, Button } from "@mantine/core";
+import { Avatar, Text, Button, Paper } from '@mantine/core';
 import { UserContext } from "../../provider/UserContext.jsx";
+import { useContext } from "react";
 import './Profile.scss';
-import { IconUser, IconMailFilled, IconEdit } from "@tabler/icons-react";
 
 export default function Profile() {
     const { user } = useContext(UserContext);
-
     // Link to the user's profile image
-    const profileImageUrl = "https://picsum.photos/200/300";
+    user.profileImageUrl = "https://picsum.photos/200/300";
 
     return (
-        <div className="profile">
-            <div className="card">
-                <div className="profile-header">
-                    <img src={profileImageUrl} alt="Profile" className="profile-picture" />
-                    <Title order={1} className="profile-username">
-                        {user.username}
-                    </Title>
-                    <Text className="profile-email">
-                        {user.email}
-                    </Text>
-                </div>
-            </div>
-
-            <div className="card">
-                <div className="profile__section">
-                    <IconUser size={18} style={{ marginRight: 8 }} />
-                    <Text>
-                        {user.username}
-                    </Text>
-                </div>
-                <div className="profile__section">
-                    <IconMailFilled size={18} style={{ marginRight: 8 }} />
-                    <Text>
-                        {user.email}
-                    </Text>
-                </div>
-            </div>
-
-            <div className="card">
-                <Title order={3} className="b-600">
-                    Update your profile
-                </Title>
-                <Text className="b-300" size="sm" color="dimmed">
-                    Update your profile information
-                </Text>
-                <Button fullWidth>
-                    <IconEdit size={18} style={{ marginRight: 8 }} />
+        <Paper className="profile-container" radius="md" withBorder p="lg" bg="var(--mantine-color-body)">
+            <Avatar
+                className="avatar"
+                src={user.profileImageUrl}
+                size={120}
+                radius={120}
+                mx="auto"
+            />
+            <Text ta="center" fz="lg" fw={500} mt="md">
+                {user.username}
+            </Text>
+            <Text ta="center" c="dimmed" fz="sm">
+                {user.email}
+            </Text>
+            <div className="button-wrapper">
+                <Button className="update-profile-button" variant="default" fullWidth mt="md">
                     Update Profile
                 </Button>
             </div>
-        </div>
+        </Paper>
     );
 }
--- packages/client/src/pages/Settings/Settings.jsx
+++ packages/client/src/pages/Settings/Settings.jsx
@@ -1,18 +1,58 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
-import { Title, Text } from "@mantine/core";
-import './Settings.scss';
+import React, { useContext } from 'react';
+import { Avatar, Button, Paper, TextInput, Title, Text } from '@mantine/core';
+import { UserContext } from '../../provider/UserContext.jsx';
+//import './Settings.scss';
 
-const Settings = () => {
+export default function Settings() {
+    const { user } = useContext(UserContext);
+
+    // Link to the user's profile image
+    user.profileImageUrl = "https://picsum.photos/200/300";
+
     return (
-        <div className="settings">
-            <div className="card">
-                <Title order={3} className={"b-600"}>Account Settings</Title>
-                <Text className={"b-300"} size="sm" color="dimmed">Modify your account information</Text>
-                {/* Ajout a faire */}
-            </div>
+        <div className="settings-container">
+            <Title order={2} className="mantine-h2">
+                Modify your account information
+            </Title>
+            <Paper className="mantine-paper" radius="md" withBorder p="lg" bg="var(--mantine-color-body)">
+                <Avatar src={user.profileImageUrl} size={120} radius={120} mx="auto" />
+                <Title order={1} className="mantine-h1" mt="md">
+                    {user.username}
+                </Title>
+                <Text className="mantine-text" mt="sm">
+                    {user.email}
+                </Text>
+
+                <div className="settings-card">
+                    <TextInput
+                        className="mantine-input"
+                        label="Username"
+                        placeholder="Enter your new username"
+                        fullWidth
+                    />
+                    <TextInput
+                        className="mantine-input"
+                        label="Email"
+                        placeholder="Enter your new email"
+                        type="email"
+                        fullWidth
+                    />
+                    <TextInput
+                        className="mantine-input"
+                        label="Password"
+                        placeholder="Enter your new password"
+                        type="password"
+                        fullWidth
+                    />
+                    <Button className="mantine-button" variant="outline" fullWidth mt="md">
+                        Update Profile
+                    </Button>
+                    <Button className="mantine-button" variant="filled" color="red" fullWidth mt="sm">
+                        Delete Account
+                    </Button>
+                </div>
+            </Paper>
         </div>
     );
 }
-
-export default Settings;
