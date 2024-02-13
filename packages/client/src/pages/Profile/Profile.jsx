d4bc91f96af27a3fa41b0c5aed0527b5e1a4c3de
Yehoudi <vincentyehoudi7@gmail.com>
2024-02-13 14:42:01 +0100

    Modification du fichier scss

--- packages/client/src/pages/Profile/Profile.jsx
+++ packages/client/src/pages/Profile/Profile.jsx
@@ -1,43 +1,51 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { Title, Text, Button } from "@mantine/core";
 import { UserContext } from "../../provider/UserContext.jsx";
-//import { Background } from "./Background.jsx";
 import './Profile.scss';
-import GenericProfileSvg from './GenericProfileSvg.jsx';
 import { IconUser, IconMailFilled, IconEdit } from "@tabler/icons-react";
 
 export default function Profile() {
-    const { user, setUser } = useContext(UserContext);
-    // Données statiques à afficher dans le profil
-    const staticData = {
-        username: "Yehoudi",
-        email: "vincentyehoudi@example.com",
-    };
+    const { user } = useContext(UserContext);
+
+    // Link to the user's profile image
+    const profileImageUrl = "https://picsum.photos/200/300";
 
     return (
         <div className="profile">
-            {/* {<Background />} */}
             <div className="card">
-                <div>
-                    <Title order={3} className={"b-600"}>
-                        {staticData.username}'s Profile
+                <div className="profile-header">
+                    <img src={profileImageUrl} alt="Profile" className="profile-picture" />
+                    <Title order={1} className="profile-username">
+                        {user.username}
                     </Title>
-                    <Text className={"b-300"} size="sm" color="dimmed">
-                        Your profile information
+                    <Text className="profile-email">
+                        {user.email}
                     </Text>
                 </div>
-                <div className="profile-info">
-                    <GenericProfileSvg />
-                    <div className="profile-field">
-                        <IconUser size={24} />
-                        <Text>{staticData.username}</Text>
-                    </div>
-                    <div className="profile-field">
-                        <IconMailFilled size={24} />
-                        <Text>{staticData.email}</Text>
-                    </div>
-                </div>
+            </div>
 
+            <div className="card">
+                <div className="profile__section">
+                    <IconUser size={18} style={{ marginRight: 8 }} />
+                    <Text>
+                        {user.username}
+                    </Text>
+                </div>
+                <div className="profile__section">
+                    <IconMailFilled size={18} style={{ marginRight: 8 }} />
+                    <Text>
+                        {user.email}
+                    </Text>
+                </div>
+            </div>
+
+            <div className="card">
+                <Title order={3} className="b-600">
+                    Update your profile
+                </Title>
+                <Text className="b-300" size="sm" color="dimmed">
+                    Update your profile information
+                </Text>
                 <Button fullWidth>
                     <IconEdit size={18} style={{ marginRight: 8 }} />
                     Update Profile
--- packages/client/src/pages/Settings/Settings.jsx
+++ packages/client/src/pages/Settings/Settings.jsx
@@ -1,12 +1,11 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { Title, Text } from "@mantine/core";
-import { BackgroundIllustration } from "../Profile/Background.jsx";
 import './Settings.scss';
 
 const Settings = () => {
     return (
         <div className="settings">
-            <BackgroundIllustration />
             <div className="card">
                 <Title order={3} className={"b-600"}>Account Settings</Title>
                 <Text className={"b-300"} size="sm" color="dimmed">Modify your account information</Text>
