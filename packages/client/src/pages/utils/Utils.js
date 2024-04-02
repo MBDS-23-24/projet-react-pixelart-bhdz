569f16597c6c44fb09085535c174be6d2c1ac2f6
pierrebihannic <70748144+pierrebihannic@users.noreply.github.com>
2024-04-02 20:40:12 +0200

    modification formatage de date en englais

--- packages/client/src/pages/utils/Utils.js
+++ packages/client/src/pages/utils/Utils.js
@@ -34,7 +34,7 @@ export function getElapsedTime(date) {
 
 export function formatedDateTime(date) {
     const options = {year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'};
-    return new Date(date).toLocaleDateString(undefined, options);
+    return new Date(date).toLocaleDateString('en', options);
 }
 
 export function sortArrayByDate(array) {
